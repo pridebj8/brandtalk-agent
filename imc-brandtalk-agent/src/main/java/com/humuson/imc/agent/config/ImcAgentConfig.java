73fe913f41f67976f514c0d77be2b73e577a4625
pridebj88 <pridebj88@gmail.com>
2020-07-30 21:07:56 +0900

    initial commit

---
+++ imc-brandtalk-agent/src/main/java/com/humuson/imc/agent/config/ImcAgentConfig.java
@@ -0,0 +1,40 @@
+package com.humuson.imc.agent.config;
+
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "imc.agent")
+@ImportResource("${imc.agent.context-path}")
+public class ImcAgentConfig {
+	
+	private boolean			showSql;
+	private boolean			sendRun;
+	private boolean			reportRun;
+	private boolean			useBtBizMsg;
+	
+	private String			messageApiHost;
+	private String			reportApiHost;
+	private String			clientId;
+	private String			apiKey;
+	
+	private String			senderCode;
+	
+	private String			bizMsgSenderKey;
+	private int				bizMsgTtl;
+	private String			bizMsgLogFormet;
+	private int				bizMsgLogMoveExpired;
+	private List<String>	bizMsgEtcColumnList;
+	private boolean			bizMsgUseKoreaCountyCode;
+	private String			btBizMsgResendMethod;
+
+}
---
+++ imc-brandtalk-agent/src/test/java/com/humuson/imc/agent/ImcBrandtalkAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humuson.imc.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcBrandtalkAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
